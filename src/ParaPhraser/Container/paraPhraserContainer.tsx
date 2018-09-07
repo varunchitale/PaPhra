352651a6249a60be1f817aa0979689891647cf00
ashish <ashish@riverus.in>
2018-09-07 10:47:23 +0530

    first commit for client app

---
+++ src/ParaPhraser/Container/paraPhraserContainer.tsx
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import ParaPhraser from '../Component/paraPhraser';
+
+export function mapStateToProps(appState: any) {
+  return {
+  };
+}
+
+export function mapDispatchToProps(dispatch: any) {
+  return {
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ParaPhraser as any);
